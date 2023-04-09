74fb20f619b7484205662e0b563b6e7636fde935
Samson Zhang <wwsamson@yahoo.com>
2023-04-09 03:06:24 -0700

    update to get current tab dom

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+function getDOM() {
+  console.log(document.body.innerHTML);
+  return document.body.innerHTML;
+}
+
 const Popup = () => {
   return (
     <div>
       <p>testing</p>
+      {chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        const activeTabId = tabs[0].id;
+        chrome.scripting.executeScript({
+          target: { tabId: activeTabId },
+          func: getDOM,
+        }, results => {
+          console.log("results", results);
+        })
+      })}
     </div>
   );
 };
